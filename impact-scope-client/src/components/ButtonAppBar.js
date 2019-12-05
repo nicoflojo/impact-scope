1cd2ffe77e2facca985ea70fffd02e12929377c4
nico <nico@pop-os.localdomain>
2019-12-05 21:10:35 +0700

    adds prettier

--- impact-scope-client/src/components/ButtonAppBar.js
+++ impact-scope-client/src/components/ButtonAppBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -9,40 +9,39 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
   root: {
-        flexGrow: 1,
+    flexGrow: 1,
   },
   menuButton: {
-        marginRight: theme.spacing(2),
-      
+    marginRight: theme.spacing(2),
   },
   title: {
-        flexGrow: 1,
-      
+    flexGrow: 1,
   },
   toolbar: {
-        background: '#056839'
-  }
-  
+    background: '#056839',
+  },
 }));
 
 export default function ButtonAppBar() {
-    const classes = useStyles();
+  const classes = useStyles();
 
   return (
-        <div className={classes.root}>
-                <AppBar position="static">
-                          <Toolbar className={classes.toolbar}>
-                                      <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                                                    <MenuIcon />
-                                                  </IconButton>
-                                      <Typography variant="h6" className={classes.title}>
-                                                    Impact Scope
-                                                  </Typography>
-                                      <Button color="inherit">Login</Button>
-                                    </Toolbar>
-                        </AppBar>
-              </div>
-      
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Impact Scope
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
-  
 }
